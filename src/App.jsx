02a50089d1db0fa672456c892508391e8147aeb6
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-06 11:07:43 -0300

    Retorno em formato de form da label e input para cada item do objeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 
 const App = () => {
-  const [nome, setNome] = React.useState('');
-  return;
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
+  return (
+    <form>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
+    </form>
+  );
 };
 
 export default App;
